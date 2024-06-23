e708ea34a09e7b1ad794a082bf9dc8affe8b429f
zhijianyang <zyan3784@uni.sydney.edu.au>
2024-06-23 12:46:28 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import Navbar from "./scenes/Navbar";
 import Landing from "./scenes/Landing";
 import DotGroup from "./scenes/DotGroup";
-import MySkills from "./scenes/MySkills";
-import LineGradient from "./components/LineGradient";
 import Projects from "./scenes/Projects";
-import Contact from "./scenes/Contact";
 import Footer from "./scenes/Footer";
 import useMediaQuery from "./hooks/useMediaQuery";
 import { useEffect, useState } from "react";
-import Testimonials from "./scenes/Testimonials";
 import { motion } from "framer-motion";
 
 function App() {
